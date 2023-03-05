a8f469c114ff8d9c958c66e2512d1f51fe1fa7cf
Gonzalo Alvarez <gonzalo@gmail.com>
2023-03-05 14:44:32 -0800

    Updated documentation to use mkdocs. updated cli to use click properly

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -7,18 +7,40 @@ import sys
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 import click
 {%- endif %}
+{%- if cookiecutter.use_loguru == 'y' %}
+from loguru import logger
+from click_loguru import ClickLoguru
+
+__program__ = '{{cookiecutter.project_slug}}'
+__version__ = '0.0.1'
+
+click_loguru = ClickLoguru(__program__, __version__)
+{%- endif %}
 
 {% if cookiecutter.command_line_interface|lower == 'click' %}
-@click.command()
-def main(args=None):
+@click.group(invoke_without_command=True)
+{%- if cookiecutter.use_loguru == 'y' %}
+@click_loguru.logging_options
+@click_loguru.stash_subcommand()
+@click_loguru.init_logger()
+{%- endif %}
+@click.version_option(prog_name=__program__, version=__version__)
+@click.pass_context
+def cli(ctx, **kwargs):
+    pass
+
+@cli.command(name='main', help='description')
+@click.pass_context
+def main(ctx):
     """Console script for {{cookiecutter.project_slug}}."""
     click.echo("Replace this message by putting your code into "
                "{{cookiecutter.project_slug}}.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 {%- endif %}
+
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
-def main():
+def cli(args=None):
     """Console script for {{cookiecutter.project_slug}}."""
     parser = argparse.ArgumentParser()
     parser.add_argument('_', nargs='*')
@@ -32,4 +54,4 @@ def main():
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(cli(obj={})) # pragma: no cover
